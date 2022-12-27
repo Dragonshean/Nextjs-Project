ae16cba05617c403bff5eb14b5ef3a482b7664be
dargonshean <dragonshean@gmail.com>
2022-12-27 15:18:08 +0800

    still working on the menu

--- components/Dishes/DisplayModal.tsx
+++ components/Dishes/DisplayModal.tsx
@@ -2,21 +2,24 @@ import Image from 'next/image'
 // import dishImage from "/images/五更腸旺.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark} from '@fortawesome/free-solid-svg-icons'
-import { closeModalProps, DishesListProps } from '../../pages/Menu/index';
+import { closeModalProps, DishProps } from '../../pages/Menu/index';
 
 
 
-const DisplayModal = ({dishes, closeModal}: DishesListProps & closeModalProps) => {
+const DisplayModal = ({ dish, closeModal }: DishProps & closeModalProps) => {
     
-    console.log(dishes)
+    console.log("Display", dish)
 
     // const { id:string, name:string, price_noon:string, price_night, img:string }[] = dishes
+
+    const basePath = '/images/熱吧食材/'
+    const imagePath = `${basePath}${dish}`
 
     return ( 
         <div className='dishModal'>
             <div className='left-img'>
                 <Image alt='dish-img' 
-                    src="/images/熱吧食材/啤酒蛤蜊.jpg" 
+                    src={imagePath} 
                     width={300} 
                     height={300} 
                     style={{
@@ -29,8 +32,10 @@ const DisplayModal = ({dishes, closeModal}: DishesListProps & closeModalProps) =
                 />
             </div>
             <div className='right-text'>
-                <h2>{`dishName`}</h2>
+                <h2>{dish.}</h2>
                 <p>{`dishInfo`}</p>
+                <p>{`平日午餐 NTD$ ${""}`}</p>
+                <p>{`平日&假日 晚餐 NTD$ ${""}`}</p>
             </div>
             <button onClick={closeModal}>
                 <FontAwesomeIcon icon={faXmark} />
